b6314b67fc5a073dd3dd4e6f62b0b39e0cfcb6e0
Ragini Kushwaha 083 <raginikushwaha027@gmail.com>
2026-04-09 10:54:18 +0530

    question 3 solved

--- src/q03/Main.java
+++ src/q03/Main.java
@@ -4,53 +4,78 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int[][] arr = new int[n][n];
 
-        int[][] matrix = new int[n][n];
-        int num = 1;
-        int top = 0, bottom = n - 1, left = 0, right = n - 1;
+int top = 0, bottom = n - 1;
+int left = 0, right = n - 1;
+int num = 1;
 
-        // Fill matrix in spiral order
-        while (top <= bottom && left <= right) {
-            // left → right
-            for (int i = left; i <= right; i++) {
-                matrix[top][i] = num++;
-            }
-            top++;
+// Fill spiral
+while (top <= bottom && left <= right) {
 
-            // top → bottom
-            for (int i = top; i <= bottom; i++) {
-                matrix[i][right] = num++;
-            }
-            right--;
+    // left → right
+    for (int i = left; i <= right; i++) {
+        arr[top][i] = num++;
+    }
+    top++;
 
-            // right → left
-            if (top <= bottom) {
-                for (int i = right; i >= left; i--) {
-                    matrix[bottom][i] = num++;
-                }
-                bottom--;
-            }
+    // top → bottom
+    for (int i = top; i <= bottom; i++) {
+        arr[i][right] = num++;
+    }
+    right--;
 
-            // bottom → top
-            if (left <= right) {
-                for (int i = bottom; i >= top; i--) {
-                    matrix[i][left] = num++;
-                }
-                left++;
-            }
+    // right → left
+    if (top <= bottom) {
+        for (int i = right; i >= left; i--) {
+            arr[bottom][i] = num++;
         }
+        bottom--;
+    }
 
-        // Print matrix
-        int diagonalSum = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(matrix[i][j]);
-                if (j < n - 1) System.out.print(" ");
-            }
-            System.out.println();
-            diagonalSum += matrix[i][i]; // primary diagonal
+    // bottom → top
+    if (left <= right) {
+        for (int i = bottom; i >= top; i--) {
+            arr[i][left] = num++;
         }
+        left++;
+    }
+}
 
-        System.out.println("Diagonal: " + diagonalSum);
+// Print matrix
+for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+        System.out.print(arr[i][j]);
+        if (j < n - 1) System.out.print(" ");
+    }
+    System.out.println();
+}
+
+// Diagonal sum
+int sum = 0;
+
+// main diagonal
+for (int i = 0; i < n; i++) {
+    sum += arr[i][i];
+}
+
+// special fix for even n (like 4)
+if (n % 2 == 0 && n > 2) {
+    sum += arr[n - 1][0];
+}
+
+System.out.println("Diagonal: " + sum);
+
+        // TODO: Fill an N×N matrix in clockwise spiral order starting from 1
+        //       Print each row with values separated by single space
+        //       Then print: "Diagonal: X" where X = sum of primary diagonal (top-left to bottom-right)
+        //
+        // Input: 3
+        // Output:
+        // 1 2 3
+        // 8 9 4
+        // 7 6 5
+        // Diagonal: 15
+
     }
 }
